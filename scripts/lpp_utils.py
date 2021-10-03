08f2aaa3400c00a361e19e56bef44bfebbf886db
rschmaelzle <schmaelz@msu.edu>
2021-10-03 11:03:19 -0400

    adding extracted, recomined files and basic code

---
+++ scripts/lpp_utils.py
@@ -0,0 +1,106 @@
+from scipy.stats import pearsonr, zscore
+import numpy as np
+
+def lpp_isc(data, pairwise=False, summary_statistic=None, verbose=True):
+    """Intersubject correlation
+    For each voxel or ROI, compute the Pearson correlation between each
+    subject's response time series and other subjects' response time series.
+    If pairwise is False (default), use the leave-one-out approach, where
+    correlation is computed between each subject and the average of the other
+    subjects. If pairwise is True, compute correlations between all pairs of
+    subjects. If summary_statistic is None, return N ISC values for N subjects
+    (leave-one-out) or N(N-1)/2 ISC values for each pair of N subjects,
+    corresponding to the upper triangle of the pairwise correlation matrix
+    (see scipy.spatial.distance.squareform). Alternatively, supply either
+    np.mean or np.median to compute summary statistic of ISCs (Fisher Z will
+    be applied and inverted if using mean). Input data should be a list 
+    where each item is a time-points by voxels ndarray for a given subject.
+    Multiple input ndarrays must be the same shape. If a single ndarray is
+    supplied, the last dimension is assumed to correspond to subjects. If 
+    only two subjects are supplied, simply compute Pearson correlation
+    (precludes averaging in leave-one-out approach, and does not apply
+    summary statistic.) Output is an ndarray where the first dimension is
+    the number of subjects or pairs and the second dimension is the number
+    of voxels (or ROIs).
+        
+    The implementation is based on the following publication:
+    
+    .. [Hasson2004] "Intersubject synchronization of cortical activity 
+    during natural vision.", U. Hasson, Y. Nir, I. Levy, G. Fuhrmann,
+    R. Malach, 2004, Science, 303, 1634-1640.
+    Parameters
+    ----------
+    data : list or ndarray
+        fMRI data for which to compute ISC
+        
+    pairwise : bool, default: False
+        Whether to use pairwise (True) or leave-one-out (False) approach
+        
+    summary_statistic : None
+        Return all ISCs or collapse using np.mean or np.median
+    Returns
+    -------
+    iscs : subjects or pairs by voxels ndarray
+        ISC for each subject or pair (or summary statistic) per voxel
+    """
+    
+    # Convert list input to 3d and check shapes
+    if type(data) == list:
+        data_shape = data[0].shape
+        for i, d in enumerate(data):
+            if d.shape != data_shape:
+                raise ValueError("All ndarrays in input list "
+                                 "must be the same shape!")
+            if d.ndim == 1:
+                data[i] = d[:, np.newaxis]
+        data = np.dstack(data)
+
+    # Convert input ndarray to 3d and check shape
+    elif type(data) == np.ndarray:
+        if data.ndim == 2:
+            data = data[:, np.newaxis, :]            
+        elif data.ndim == 3:
+            pass
+        else:
+            raise ValueError("Input ndarray should have 2 "
+                             f"or 3 dimensions (got {data.ndim})!")
+
+    # Infer subjects, TRs, voxels and print for user to check
+    n_subjects = data.shape[2]
+    n_TRs = data.shape[0]
+    n_voxels = data.shape[1]
+    if verbose:
+        print(f"Assuming {n_subjects} subjects with {n_TRs} time points "
+              f"and {n_voxels} voxel(s) or ROI(s).")
+    
+    # Loop over each voxel or ROI
+    voxel_iscs = []
+    for v in np.arange(n_voxels):
+        voxel_data = data[:, v, :].T
+        if n_subjects == 2:
+            iscs = pearsonr(voxel_data[0, :], voxel_data[1, :])[0]
+            summary_statistic = None
+            if verbose:
+                print("Only two subjects! Simply computing Pearson correlation.")
+        elif pairwise:
+            iscs = squareform(np.corrcoef(voxel_data), checks=False)
+        elif not pairwise:
+            iscs = np.array([pearsonr(subject,
+                                      np.mean(np.delete(voxel_data,
+                                                        s, axis=0),
+                                              axis=0))[0]
+                    for s, subject in enumerate(voxel_data)])
+        voxel_iscs.append(iscs)
+    iscs = np.column_stack(voxel_iscs)
+    
+    # Summarize results (if requested)
+    if summary_statistic == np.mean:
+        iscs = np.tanh(summary_statistic(np.arctanh(iscs), axis=0))[np.newaxis, :]
+    elif summary_statistic == np.median:    
+        iscs = summary_statistic(iscs, axis=0)[np.newaxis, :]
+    elif not summary_statistic:
+        pass
+    else:
+        raise ValueError("Unrecognized summary_statistic! Use None, np.median, or np.mean.")
+    return iscs
+
